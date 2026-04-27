6328d15a55fcaf8c1270cac4ed11cb9ea1eaa2ee
Manu <83924182+CosmeManu25@users.noreply.github.com>
2026-04-27 15:29:33 -0300

    Refactor amigos function and adjust limits

--- ejemplo7.py
+++ ejemplo7.py
@@ -2,34 +2,32 @@ from decorators import delta_time
 
 @delta_time("GRUPO GN")
 def amigos(tope):
-    import array
+    import array 
     
-    limite_extendido = int(tope * 1.3)
+    limite_extendido = int(tope * 1.2)
     limite_mitad = (limite_extendido // 2) + 1
-    
     stop_criba = limite_extendido + 1 
     stop_busqueda = tope + 1
-    
-    sumas_multiplos = array.array('I', [1] * stop_criba)
-    
+
+    sumas_multiplos = array.array('I', [1]) * stop_criba
+
     for i in range(2, limite_mitad):
-        inicio_j = i * 2
-        for j in range(inicio_j, stop_criba, i):
+
+        for j in range(i*2, stop_criba, i):
+
             sumas_multiplos[j] += i
 
     amigos = []
     add_amigo = amigos.append
-    
+
     for i in range(2, stop_busqueda):
         posible_amigo = sumas_multiplos[i]
         
-        if i < posible_amigo < stop_criba:
-            if sumas_multiplos[posible_amigo] == i:
-                add_amigo(i)
-                add_amigo(posible_amigo)
+        if i < posible_amigo < stop_criba and sumas_multiplos[posible_amigo] == i:
+            add_amigo(i)
+            add_amigo(posible_amigo)
                 
     return amigos
-
 
 if __name__ == "__main__":
     t = int(input("Ingrese tope: "))
